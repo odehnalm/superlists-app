054337b7db3c92b0b2cb41115f826c0120b181b7
Marco <markodehnal@gmail.com>
2017-11-21 18:03:59 -0400

    new URL + view for adding to existing lists. FT passes :-)

--- functional_tests/tests.py
+++ functional_tests/tests.py
@@ -53,14 +53,14 @@ class NewVisitorTest(LiveServerTestCase):
         # "1: Buy peacock feathers" as an item in a to-do list table
         inputbox.send_keys(Keys.ENTER)
         self.wait_for_row_in_list_table("1: Buy peacock feathers")
-        
+
         #There is still a text box inviting her to add another item. She
         #enters "Use a peacock feather to make a fly" (Edith is very
         #methodical
         inputbox = self.browser.find_element_by_id("id_new_item")
         inputbox.send_keys("Use peacock feathers to make a fly")
         inputbox.send_keys(Keys.ENTER)
-        
+
         #The page updates again, and now shows both items on her list
         self.wait_for_row_in_list_table("1: Buy peacock feathers")
         self.wait_for_row_in_list_table("2: Use peacock feathers to make a fly")
@@ -71,7 +71,7 @@ class NewVisitorTest(LiveServerTestCase):
         #self.fail('Finish the Test!')
 
     def test_multiple_users_can_start_lists_at_different_urls(self):
-        #Edith starts a new to-do list
+        # Edith starts a new to-do list
         self.browser.get(self.live_server_url)
         inputbox = self.browser.find_element_by_id('id_new_item')
         inputbox.send_keys('Buy peacock feathers')
@@ -82,34 +82,34 @@ class NewVisitorTest(LiveServerTestCase):
         edith_list_url = self.browser.current_url
         self.assertRegex(edith_list_url,'/lists/.+')
 
-        #Now, a new user, Francis,  comes along to the site.
+        # Now a new user, Francis, comes along to the site.
 
-        ##We use a new browser session to make sure that no information
-        ##of Edith's is coming throough from cookies, etc
+        ## We use a new browser session to make sure that no information
+        ## of Edith's is coming through from cookies etc
         self.browser.quit()
         self.browser = webdriver.Firefox()
 
-        #Francis visits the homepage. There is no sign of Edith's list
+        # Francis visits the home page.  There is no sign of Edith's
+        # list
         self.browser.get(self.live_server_url)
         page_text = self.browser.find_element_by_tag_name('body').text
-        self.assertNotIn('Buy peacock feathers',page_text)
-        self.assertNotIn('make a fly',page_text)
+        self.assertNotIn('Buy peacock feathers', page_text)
+        self.assertNotIn('make a fly', page_text)
 
-        #Francis starts a new list by entering a new item. He is less
-        #interesing than Edith...
+        # Francis starts a new list by entering a new item. He
+        # is less interesting than Edith...
         inputbox = self.browser.find_element_by_id('id_new_item')
         inputbox.send_keys('Buy milk')
         inputbox.send_keys(Keys.ENTER)
         self.wait_for_row_in_list_table('1: Buy milk')
 
-        #Francis gets his own unique URL
+        # Francis gets his own unique URL
         francis_list_url = self.browser.current_url
-        self.assertRegex(francis_list_url,'/lists/.+')
-        self.assertNotEqual(francis_list_url,edith_list_url)
+        self.assertRegex(francis_list_url, '/lists/.+')
+        self.assertNotEqual(francis_list_url, edith_list_url)
 
-        #Again, there is no trace of Edith's list
+        # Again, there is no trace of Edith's list
         page_text = self.browser.find_element_by_tag_name('body').text
         self.assertNotIn('Buy peacock feathers', page_text)
-        self.assertIn('Buy milk',page_text)
-
-        #Satisfied, they both go back to sleep
+        self.assertIn('Buy milk', page_text)
+        # Satisfied, they both go back to sleep
